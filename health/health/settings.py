898bd5e40bd7a6bf54c6850285201af36efdf7e2
D-A-D-R-F <69009528+D-A-D-R-F@users.noreply.github.com>
2021-09-13 18:35:04 +0530

    Update settings.py

--- health/health/settings.py
+++ health/health/settings.py
@@ -133,8 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'davis.health.help@gmail.com'
-EMAIL_HOST_PASSWORD = 'luqbqyibnikayjfg'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PWD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Healthelp'
